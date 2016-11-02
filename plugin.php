136c4eab86d9b1a20dc480a8abeeae6135e97646
Tomasz <thomas.kolonko@ilub.unibe.ch>
2016-11-02 09:37:21 +0100

    ilias52 ready

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'shortlink';
 $version = '0.0.8';
 $ilias_min_version = '4.3.0';
-$ilias_max_version = '5.1.999';
+$ilias_max_version = '5.2.999';
 $responsible = 'Timon Amstutz';
 $responsible_mail = 'timon.amstutz@ilub.unibe.ch';
