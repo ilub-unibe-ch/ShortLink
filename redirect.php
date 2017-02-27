9b3dafbf78df02f8cdfbe1a1193e9a219d9218f4
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2017-02-27 14:19:59 +0100

    added Description to redirect.php

--- redirect.php
+++ redirect.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * This redirect file is called when a ShortLink with its prefix is entered inside the browsers address field.
+ * It fetches the full url from the DB and redirects the ShortLink to the full URI.
+ *
  * Created by PhpStorm.
  * User: tomasz
  * Date: 16/01/16
