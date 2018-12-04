e1edd226b24129ed8ccba4d0ed7254627921edce
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2018-12-04 13:03:23 +0100

    updated for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'shortlink';
 $version = '1.0.2';
 $ilias_min_version = '4.3.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Tomasz Kolonko';
 $responsible_mail = 'thomas.kolonko@ilub.unibe.ch';
