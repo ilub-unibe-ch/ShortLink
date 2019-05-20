a9d728fbfd4e2ffe11d437aac4d510e41d90e967
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2019-05-20 14:48:02 +0200

    Update responsible e-mail

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'shortlink';
 $version = '1.0.3';
 $ilias_min_version = '4.3.0';
 $ilias_max_version = '5.4.999';
-$responsible = 'Tomasz Kolonko';
-$responsible_mail = 'thomas.kolonko@ilub.unibe.ch';
+$responsible = 'Olivia Kaufmann';
+$responsible_mail = 'olivia.kaufmann@ilub.unibe.ch';
